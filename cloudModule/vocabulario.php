f2f932493136a50cee2f0bc5d26f807c414b02e2
Emiliano <emilianoalejandrocalderon@gmail.com>
2024-11-29 12:56:14 -0500

    retroalimentacion1/2 bd

--- cloudModule/vocabulario.php
+++ cloudModule/vocabulario.php
@@ -10,9 +10,9 @@
 <?php
 // Configuración de la base de datos
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "adsoPage";
+$username = "u469734594_adsoPageRoot";
+$password = "pgAdso2024";
+$dbname = "u469734594_adsoPage";
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
